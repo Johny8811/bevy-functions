30455d42736e9fbe0f39376018ef35cd74f7f849
Jan Hujsa <jan.hujsa@gmail.com>
2022-08-21 22:46:14 +0200

    Add dependencies, add to gitignore, adjust eslint and typescript

--- functions/.eslintrc.js
+++ functions/.eslintrc.js
@@ -27,5 +27,7 @@ module.exports = {
   rules: {
     "quotes": ["error", "double"],
     "import/no-unresolved": 0,
+    "object-curly-spacing": [2, "always"],
+    "new-cap": ["error", { "capIsNewExceptionPattern": "^express\\.." }],
   },
 };
