3b72cecc713ed9a29e476c0ef4cfc0599a11785c
Jan Hujsa <jan.hujsa@gmail.com>
2022-08-22 12:58:02 +0200

    Add route to get prepared tasks for next day

--- functions/src/routers/onfleet/db.ts
+++ functions/src/routers/onfleet/db.ts
@@ -1,6 +1,7 @@
 import { OnfleetTask } from "@onfleet/node-onfleet/Resources/Tasks";
 
 import { client } from "../../integrations/mongodb";
+import { getNextDayTimeValues } from "../utils";
 
 const tasksCollection = client
     .db("on_fleet")
@@ -10,3 +11,13 @@ export const insertTasks = (tasks: OnfleetTask[]) => tasksCollection.insertMany(
     tasks,
     { ordered: true }
 );
+
+export const getAllTasks = ({
+  from,
+  completeAfterAfter,
+  completeBeforeBefore,
+}: ReturnType<typeof getNextDayTimeValues>) => tasksCollection.find<OnfleetTask>({
+  timeCreated: { $gt: from },
+  completeAfter: { $gt: completeAfterAfter },
+  completeBefore: { $lt: completeBeforeBefore },
+}).toArray();
--- functions/src/routers/onfleet/index.ts
+++ functions/src/routers/onfleet/index.ts
@@ -1,10 +1,10 @@
 import { logger } from "firebase-functions";
 import * as express from "express";
 
-import { getAllTasks } from "../../integrations/onFleet/getAllTasks";
+import { getAllTasks as onFleetGetAllTasks } from "../../integrations/onFleet/getAllTasks";
 import { getNextDayTimeValues } from "../utils";
 
-import { insertTasks } from "./db";
+import { insertTasks, getAllTasks } from "./db";
 
 export const onFleetRouter = express.Router();
 
@@ -13,10 +13,10 @@ onFleetRouter.get(
     async (req, res) => {
       try {
         const timeValues = getNextDayTimeValues();
-        const tasks = await getAllTasks(timeValues);
+        const tasks = await onFleetGetAllTasks(timeValues);
 
         logger.log(
-            "Prepared tasks ids for next day: ",
+            "/export/saveToDb - Prepared tasks ids for next day: ",
             tasks.map((task) => task.id)
         );
 
@@ -28,7 +28,7 @@ onFleetRouter.get(
       } catch (e) {
         // TODO: improve error handling and logging
         //  https://kentcdodds.com/blog/get-a-catch-block-error-message-with-typescript
-        logger.log("Route:/export/saveToDb - Error: ", e);
+        logger.log("/export/saveToDb - Error: ", e);
         res.status(500).json({
           error: {
             message: (e as Error).message,
@@ -37,3 +37,26 @@ onFleetRouter.get(
       }
     },
 );
+
+onFleetRouter.get("/all/nextDay", async (req, res) => {
+  try {
+    const timeValues = getNextDayTimeValues();
+    const tasks = await getAllTasks(timeValues);
+
+    logger.log(
+        "/export/saveToDb - Prepared tasks ids for next day: ",
+        tasks.map((task) => task.id)
+    );
+
+    res.status(200).json(tasks);
+  } catch (e) {
+    // TODO: improve error handling and logging
+    //  https://kentcdodds.com/blog/get-a-catch-block-error-message-with-typescript
+    logger.log("Route:/export/saveToDb - Error: ", e);
+    res.status(500).json({
+      error: {
+        message: (e as Error).message,
+      },
+    });
+  }
+});
