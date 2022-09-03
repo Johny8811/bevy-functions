7136c580e16d8031a1a95f09a3aa49fd22f4541a
Jan Hujsa <jan.hujsa@gmail.com>
2022-09-03 21:14:47 +0200

    Change onFleet typing and add todo

--- functions/src/types/tasks.ts
+++ functions/src/types/tasks.ts
@@ -1,10 +1,11 @@
-import { GetTaskResult } from "@onfleet/node-onfleet/Resources/Tasks";
+import { UpdateTaskResult } from "@onfleet/node-onfleet/Resources/Tasks";
 
+// TODO: investigate onFleet types, module "@onfleet/node-onfleet" has bad coverage of onFleet types
 export type OurOnFleetTask = {
   slot: {
     start: number,
     end: number
   } | null
-} & GetTaskResult
+} & UpdateTaskResult
 
-export type OriginOnFleetTask = GetTaskResult
+export type OriginOnFleetTask = UpdateTaskResult
