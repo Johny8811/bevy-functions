f7494950f0ac9d2f3ad72b053134bdd561c037aa
Jan Hujsa <jan.hujsa@gmail.com>
2022-09-03 18:04:27 +0200

    Add util to generate hourly time slot

---
+++ functions/src/routers/utils/generateHourlyTimeSlot.ts
@@ -0,0 +1,40 @@
+import { logger } from "firebase-functions";
+import { getHours, set, isBefore, add, getTime } from "date-fns";
+
+import { OriginOnFleetTask } from "../../types/tasks";
+
+export const generateHourlyTimeSlot = (taskResult: OriginOnFleetTask) => {
+  const ect = taskResult.estimatedCompletionTime;
+
+  if (ect) {
+    const ectHour = getHours(ect);
+    const ectHalfHour = set(ect, {
+      hours: ectHour,
+      minutes: 30,
+      seconds: 0,
+      milliseconds: 0,
+    });
+
+    const isInFirstHalfHour = isBefore(ect, ectHalfHour);
+
+    const start = getTime(add(set(ect, {
+      hours: ectHour,
+      minutes: 0,
+      seconds: 0,
+      milliseconds: 0,
+    }), { minutes: isInFirstHalfHour ? 0 : 30 }));
+    const end = getTime(add(start, { hours: 1 }));
+
+    logger.log("generateTimeSlot:{ start, end } ", {
+      start,
+      end,
+    });
+
+    return {
+      start,
+      end,
+    };
+  }
+
+  return null;
+};
