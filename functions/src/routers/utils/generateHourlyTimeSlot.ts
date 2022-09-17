864a30a1114e390b82c26c64ac276ffbe1612e45
Jan Hujsa <jan.hujsa@gmail.com>
2022-09-17 11:51:46 +0200

    Fix slot generating

--- functions/src/routers/utils/generateHourlyTimeSlot.ts
+++ functions/src/routers/utils/generateHourlyTimeSlot.ts
@@ -1,10 +1,12 @@
-import { getHours, set, isBefore, add, getTime } from "date-fns";
+import { getTime, getHours, set, add, isBefore, isAfter } from "date-fns";
 
 import { OriginOnFleetTask } from "../../types/tasks";
 
 // TODO: add tests
 export const generateHourlyTimeSlot = (task: OriginOnFleetTask) => {
   const eat = task.estimatedArrivalTime;
+  const completeAfter = task.completeAfter;
+  const completeBefore = task.completeBefore;
 
   if (eat) {
     const eatHour = getHours(eat);
@@ -17,13 +19,16 @@ export const generateHourlyTimeSlot = (task: OriginOnFleetTask) => {
 
     const isInFirstHalfHour = isBefore(eat, eatHalfHour);
 
-    const start = getTime(add(set(eat, {
+    const startTime = getTime(add(set(eat, {
       hours: eatHour,
       minutes: 0,
       seconds: 0,
       milliseconds: 0,
     }), { minutes: isInFirstHalfHour ? 0 : 30 }));
-    const end = getTime(add(start, { hours: 1 }));
+    const endTime = getTime(add(startTime, { hours: 1 }));
+
+    const start = isBefore(startTime, completeAfter) ? completeAfter : startTime;
+    const end = isAfter(endTime, completeBefore) ? completeBefore : endTime;
 
     return {
       start,
