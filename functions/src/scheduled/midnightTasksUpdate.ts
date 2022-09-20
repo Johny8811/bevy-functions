7da9348c7abb008f21f7407862849dd66eb330be
Jan Hujsa <jan.hujsa@gmail.com>
2022-09-20 23:46:01 +0200

    Update required delivery time in onFleet at midnight update, catch possible errors, improve collecting of promise result

--- functions/src/scheduled/midnightTasksUpdate.ts
+++ functions/src/scheduled/midnightTasksUpdate.ts
@@ -1,18 +1,38 @@
 import { logger } from "firebase-functions";
 
-import { updateTaskByIdMidnightFields } from "../routers/tasks/db";
+import { findTasksByIDs, updateTaskByIdMidnightFields } from "../routers/tasks/db";
 import { getAllTasks as onFleetGetAllTasks } from "../integrations/onFleet/getAllTasks";
 import { todayTasks as todayTasksFilter } from "../integrations/onFleet/filters/todayTasks";
+import { onFleetApi } from "../integrations/onFleet";
 
 export const midnightTasksUpdate = async () => {
-  const filter = todayTasksFilter();
-  const onFleetTasks = await onFleetGetAllTasks(filter);
-  const exportedTasksIds = onFleetTasks.map((t) => t.id);
+  try {
+    const filter = todayTasksFilter();
+    const onFleetTasks = await onFleetGetAllTasks(filter);
+    const exportedTasksIds = onFleetTasks.map((t) => t.id);
 
-  logger.log("midnightTasksUpdate:exportedTasksIds: ", exportedTasksIds);
+    logger.log("midnightTasksUpdate:exportedTasksIds: ", exportedTasksIds);
 
-  onFleetTasks.forEach((task) => {
-    updateTaskByIdMidnightFields(task);
-  });
+
+    const updateResult = await Promise.all(onFleetTasks.map((task) =>
+      updateTaskByIdMidnightFields(task)
+    ));
+
+    const matchedCount = updateResult.map((r) => r.matchedCount);
+    const modifiedCounts = updateResult.map((r) => r.modifiedCount);
+
+    logger.log("midnightTasksUpdate:matchedCount:  ", matchedCount);
+    logger.log("midnightTasksUpdate:modifiedCounts:  ", modifiedCounts);
+
+    const databaseTasks = await findTasksByIDs(exportedTasksIds);
+    await Promise.all(databaseTasks.map((task) =>
+      onFleetApi.tasks.update(task.id, {
+        completeAfter: task.slot?.start,
+        completeBefore: task.slot?.end,
+      })
+    ));
+  } catch (e) {
+    logger.log("midnightTasksUpdate:Error:  ", e);
+  }
 };
 
