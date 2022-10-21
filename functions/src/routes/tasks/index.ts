14684fe038b86d81d7d06877c4a1f0433b2feee4
Jan Hujsa <jan.hujsa@gmail.com>
2022-10-21 02:31:51 +0200

    Move export onfleet tasks to separate file

--- functions/src/routes/tasks/index.ts
+++ functions/src/routes/tasks/index.ts
@@ -1,12 +1,6 @@
 import { logger } from "firebase-functions";
 import * as express from "express";
 
-import { getAllTasks as onFleetGetAllTasks } from "../../integrations/onFleet/getAllTasks";
-import { tomorrowTasks as tomorrowTasksFilter } from "../../integrations/onFleet/filters/tomorrowTasks";
-import { generateHourlyTimeSlot } from "../utils/generateHourlyTimeSlot";
-import { generateOrderForTasks } from "../utils/generateTaskOrder";
-import { asyncForEach } from "../../utils/asyncForEach";
-import { filterOnFleetExportByDbTasks } from "../utils/filterOnFleetExportByDbTasks";
 import { sortByWorkerAndEat } from "../utils/sortByWorkerAndEat";
 import { getValueByParameterName, hasRole } from "../../integrations/firebase/remoteConfig";
 import { RemoteConfigParameters } from "../../integrations/firebase/types";
@@ -16,10 +10,7 @@ import { withAuthorization } from "../../middlewares/withAuthorization";
 import {
   findTasksByDateAndUserId,
   findTasksByDateRage,
-  findTasksByIDs,
   findTomorrowTasks,
-  insertTasks,
-  updateTask,
 } from "./db";
 
 export const tasksRouter = express.Router();
@@ -87,58 +78,3 @@ export const getTasks = withMiddleware(withAuthorization(async (req, res) => {
     res.status(500).json({ message: (e as Error).message });
   }
 }));
-
-
-/**
- * Fetch tasks from onFleet planned for next day and save them to tasks database
- */
-// TODO: route has to be under role access
-tasksRouter.get(
-    "/onFleet/export/saveToDb",
-    async (req, res) => {
-      try {
-        const filter = tomorrowTasksFilter();
-        const onFleetTasks = await onFleetGetAllTasks(filter);
-        const exportedTasksIds = onFleetTasks.map((t) => t.id);
-
-        logger.log(
-            "Route:/onFleet/export/saveToDb - Prepared tasks ids for tomorrow: ",
-            exportedTasksIds,
-            " count: ", exportedTasksIds.length
-        );
-
-        if (onFleetTasks.length > 0) {
-          const databaseTasks = await findTasksByIDs(exportedTasksIds);
-          const tasksWithOrder = generateOrderForTasks(onFleetTasks);
-          const ourOnFleetTasks = tasksWithOrder.map((t) => ({ ...t, slot: generateHourlyTimeSlot(t) }));
-
-          const { newTasks, updatedTasks } = filterOnFleetExportByDbTasks(ourOnFleetTasks, databaseTasks);
-
-          logger.log("Route:/onFleet/export/saveToDb - new tasks ids: ", newTasks.map((t) => t.id));
-          logger.log("Route:/onFleet/export/saveToDb - updated tasks ids: ",
-              updatedTasks.map((t) => t.id)
-          );
-
-          if (newTasks.length > 0) {
-            await insertTasks(newTasks);
-          }
-
-          // TODO: Promise.all
-          if (updatedTasks.length > 0) {
-            await asyncForEach(updatedTasks, async (task) => {
-              await updateTask(task);
-            });
-          }
-
-          res.status(200).json(ourOnFleetTasks);
-        } else {
-          res.status(200).json([]);
-        }
-      } catch (e) {
-      // TODO: improve error handling and logging
-      //  https://kentcdodds.com/blog/get-a-catch-block-error-message-with-typescript
-        logger.log("Route:/onFleet/export/saveToDb - Error: ", e);
-        res.status(500).json({ message: (e as Error).message });
-      }
-    },
-);
