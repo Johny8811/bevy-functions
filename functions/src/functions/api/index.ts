f6d200e61a44cbc725184ef529460c2bd0feb43b
Jan Hujsa <jan.hujsa@gmail.com>
2022-12-09 15:00:56 +0100

    Prepared TODOS

--- functions/src/functions/api/index.ts
+++ functions/src/functions/api/index.ts
@@ -5,12 +5,16 @@ import { withAuthorization } from "../../middlewares/withAuthorization";
 import { generateKey } from "../utils/generateKey";
 import { client } from "../../integrations/postgresql";
 
-export const api = withCors(withAuthorization(async (req, res) => {
+export const api = withCors(async (req, res) => {
   try {
-    // const tasks = JSON.parse(req.body);
+    const tasks = JSON.parse(req.body);
 
-    // logger.log("tasks: ", tasks);
-    res.status(201).json(["test"]);
+    // TODO: 1. validate
+    // TODO: 2. check if batch exist
+    // TODO: 3. upload to batches order
+    // TODO: 4. upload to orders
+
+    res.status(201).json(tasks);
     // TODO: save to DB
   } catch (e) {
     // TODO: improve error handling and logging
@@ -18,7 +22,7 @@ export const api = withCors(withAuthorization(async (req, res) => {
     logger.log("api: ", e);
     res.status(500).json({ message: (e as Error).message });
   }
-}));
+});
 
 export const generateApiKey = withCors(withAuthorization(async (req, res) => {
   try {
