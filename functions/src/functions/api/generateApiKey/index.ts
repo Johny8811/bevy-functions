da16d95fd4d0cde6280e323966b8b366bf095bdf
Jan Hujsa <jan.hujsa@gmail.com>
2023-01-09 15:17:03 +0100

    Remake genearting api kys - just show keys

--- functions/src/functions/api/generateApiKey/index.ts
+++ functions/src/functions/api/generateApiKey/index.ts
@@ -2,7 +2,7 @@ import { logger } from "firebase-functions";
 
 import { withCors } from "../../../middlewares/withCors";
 import { withAuthorization } from "../../../middlewares/withAuthorization";
-import { client } from "../../../integrations/postgresql";
+// import { client } from "../../../integrations/postgresql";
 
 import { generateKey } from "../../utils/generateKey";
 
@@ -11,16 +11,17 @@ export const generateApiKey = withCors(withAuthorization(async (req, res) => {
     const carrierEmail = req.query.email && String(req.query.email);
     const generatedKey = generateKey();
 
-    if (!carrierEmail) {
-      res.status(400).json({ message: "Missing route query parameters 'email'" });
-      return;
-    }
+    // if (!carrierEmail) {
+    //   res.status(400).json({ message: "Missing route query parameters 'email'" });
+    //   return;
+    // }
 
-    logger.log("carrierEmail: ", carrierEmail);
+    logger.log("carrierEmail: ", carrierEmail, generatedKey);
 
-    const text = "UPDATE carriers SET api_key = $1 WHERE carriers.email = $2";
-    const values = [generatedKey, carrierEmail];
-    await client.query(text, values);
+    // TODO: for now this serve like generator of api keys
+    // const text = "UPDATE carriers SET api_key = $1 WHERE carriers.email = $2";
+    // const values = [generatedKey, carrierEmail];
+    // await client.query(text, values);
 
     res.status(201).json({ message: "success" });
   } catch (e) {
