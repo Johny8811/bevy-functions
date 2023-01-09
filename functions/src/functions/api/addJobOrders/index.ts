23afa73c88e3177dff9052215dd2f5957f5bf688
Jan Hujsa <jan.hujsa@gmail.com>
2023-01-09 12:02:58 +0100

    Remake file structure

---
+++ functions/src/functions/api/addJobOrders/index.ts
@@ -0,0 +1,33 @@
+import Ajv from "ajv";
+import { logger } from "firebase-functions";
+
+import { withCors } from "../../../middlewares/withCors";
+import { withApiAuthorization } from "../../../middlewares/withApiAuthorization";
+
+import { insertBatch, Task } from "../../tasks/all_tasks_db";
+import { tasksArraySchema } from "./tasksArraySchema";
+
+const schemaValidator = new Ajv();
+
+export const addJobOrders = withCors(withApiAuthorization(async (req, res) => {
+  try {
+    const tasks: Task[] = JSON.parse(req.body);
+
+    const tasksSchemaValidator = schemaValidator.compile(tasksArraySchema);
+    const valid = tasksSchemaValidator(tasks);
+
+    if (!valid) {
+      logger.log("Route:/bevy-api - validation errors: ", tasksSchemaValidator.errors);
+      res.status(400).json(tasksSchemaValidator.errors);
+      return;
+    }
+
+    await insertBatch(tasks);
+    res.status(200).json({ message: "success" });
+  } catch (e) {
+    // TODO: improve error handling and logging
+    //  https://kentcdodds.com/blog/get-a-catch-block-error-message-with-typescript
+    logger.log("api: ", e);
+    res.status(500).json({ message: (e as Error).message });
+  }
+}));
---
+++ functions/src/functions/api/generateApiKey.ts
@@ -0,0 +1,32 @@
+import { logger } from "firebase-functions";
+
+import { withCors } from "../../middlewares/withCors";
+import { withAuthorization } from "../../middlewares/withAuthorization";
+import { client } from "../../integrations/postgresql";
+
+import { generateKey } from "../utils/generateKey";
+
+export const generateApiKey = withCors(withAuthorization(async (req, res) => {
+  try {
+    const carrierEmail = req.query.email && String(req.query.email);
+    const generatedKey = generateKey();
+
+    if (!carrierEmail) {
+      res.status(400).json({ message: "Missing route query parameters 'email'" });
+      return;
+    }
+
+    logger.log("carrierEmail: ", carrierEmail);
+
+    const text = "UPDATE carriers SET api_key = $1 WHERE carriers.email = $2";
+    const values = [generatedKey, carrierEmail];
+    await client.query(text, values);
+
+    res.status(201).json({ message: "success" });
+  } catch (e) {
+    // TODO: improve error handling and logging
+    //  https://kentcdodds.com/blog/get-a-catch-block-error-message-with-typescript
+    logger.log("generateApiKey: ", e);
+    res.status(500).json({ message: (e as Error).message });
+  }
+}));
