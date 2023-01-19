095dd739435cbb6b92a7e0f7c0738fe4ed568055
Jan Hujsa <jan.hujsa@gmail.com>
2023-01-19 23:07:05 +0100

    Fix getting correct yesr & mont param to get correct data

--- functions/src/scheduled/rohlik/index.ts
+++ functions/src/scheduled/rohlik/index.ts
@@ -1,6 +1,6 @@
 import * as functions from "firebase-functions";
 import axios from "axios";
-import { getYear, getMonth, subDays } from "date-fns";
+import { getYear, getMonth, getDate, subDays, subMonths, subYears } from "date-fns";
 
 import { getEnvVariableOrExit } from "../../utils/getEnvVariableOrExit";
 import { csvToJsonStyle } from "../../utils/csvToJsonStyle";
@@ -18,10 +18,29 @@ export const getCouriersDataAndSaveToDb = functions
     .onRun(async (context) => {
       functions.logger.log("getCouriersDataAndSaveToDb:context ", context);
 
-      const currentYear = getYear(new Date());
-      const currentMonth = getMonth(new Date());
-      const currentMonthString = ("0" + (currentMonth + 1)).slice(-2);
-      const getDataDate = `${currentYear}${currentMonthString}`;
+      // TODO: MISSING TESTS!!! move this to test
+      // const testDate = new Date("2023-01-01T00:00:04.983Z"); // result should: "202212"
+      // const testDate = new Date("2023-01-02T00:00:04.983Z"); // result should: "202301"
+      // const testDate = new Date("2023-01-09T00:00:04.983Z"); // result should: "202301"
+      // const testDate = new Date("2023-01-31T00:00:04.983Z"); // result should: "202301"
+      // const testDate = new Date("2023-02-01T00:00:04.983Z"); // result should: "202301"
+      // const testDate = new Date("2023-02-02T00:00:04.983Z"); // result should: "202302"
+
+      const dateNow = new Date(); // testDate;
+
+      const currentDay = getDate(dateNow);
+      const isFirstDayOfMonth = currentDay === 1;
+
+      const currentMonth = getMonth(dateNow);
+      const isFirstMonthOfYear = currentMonth === 0;
+
+      const yearParam = isFirstMonthOfYear && isFirstDayOfMonth ?
+        getYear(subYears(dateNow, 1)) :
+        getYear(dateNow);
+      const monthParam = isFirstDayOfMonth ? getMonth(subMonths(dateNow, 1)) : getMonth(dateNow);
+
+      const monthParamStr = ("0" + (monthParam + 1)).slice(-2);
+      const getDataDate = `${yearParam}${monthParamStr}`;
 
       try {
         const invoicingUrl = `${rohlikUrl}/invoicing/?key=${rohlikToken}&carrier=BEVY&month=${getDataDate}`;
