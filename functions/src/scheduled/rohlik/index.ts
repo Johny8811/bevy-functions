cbcda6aedebc80912d1cf1cd0b245c29c71cda9e
Jan Hujsa <jan.hujsa@gmail.com>
2023-01-24 14:34:54 +0100

    Add scheduled job to get and save courier reports from Plzen

--- functions/src/scheduled/rohlik/index.ts
+++ functions/src/scheduled/rohlik/index.ts
@@ -1,78 +1,36 @@
 import * as functions from "firebase-functions";
-import axios from "axios";
-import { getYear, getMonth, getDate, subDays, subMonths, subYears } from "date-fns";
 
-import { getEnvVariableOrExit } from "../../utils/getEnvVariableOrExit";
-import { csvToJsonStyle } from "../../utils/csvToJsonStyle";
-import { insertData } from "../../database/rohlik";
-import { RohlikData } from "../../database/types";
+import { pragReportsInsertData, plzenReportsInsertData } from "../../database/rohlik";
+import { BevyRohlikCarriers, getReportsData } from "./getReportsData";
 
-const rohlikUrl = getEnvVariableOrExit("ROHLIK_URL");
-const rohlikToken = getEnvVariableOrExit("ROHLIK_TOKEN");
-
-export const getCouriersDataAndSaveToDb = functions
+export const getCourierReportsAndSavePrague = functions
     .region("europe-west3")
     .pubsub
     .schedule("00 03 * * *")
     .timeZone("Europe/Prague")
     .onRun(async (context) => {
-      functions.logger.log("getCouriersDataAndSaveToDb:context ", context.timestamp);
-
-      // TODO: MISSING TESTS!!! move this to test
-      // const testDate = new Date("2023-01-01T00:00:04.983Z"); // result should: "202212"
-      // const testDate = new Date("2023-01-02T00:00:04.983Z"); // result should: "202301"
-      // const testDate = new Date("2023-01-09T00:00:04.983Z"); // result should: "202301"
-      // const testDate = new Date("2023-01-31T00:00:04.983Z"); // result should: "202301"
-      // const testDate = new Date("2023-02-01T00:00:04.983Z"); // result should: "202301"
-      // const testDate = new Date("2023-02-02T00:00:04.983Z"); // result should: "202302"
-
-      const dateNow = new Date(context.timestamp); // testDate;
-
-      const currentDay = getDate(dateNow);
-      const isFirstDayOfMonth = currentDay === 1;
-
-      const currentMonth = getMonth(dateNow);
-      const isFirstMonthOfYear = currentMonth === 0;
-
-      const yearParam = isFirstMonthOfYear && isFirstDayOfMonth ?
-        getYear(subYears(dateNow, 1)) :
-        getYear(dateNow);
-      const monthParam = isFirstDayOfMonth ? getMonth(subMonths(dateNow, 1)) : getMonth(dateNow);
-
-      const monthParamStr = ("0" + (monthParam + 1)).slice(-2);
-      const getDataDate = `${yearParam}${monthParamStr}`;
+      functions.logger.log("getCourierReportsAndSavePrague:context ", context.timestamp);
 
       try {
-        const invoicingUrl = `${rohlikUrl}/invoicing/?key=${rohlikToken}&carrier=BEVY&month=${getDataDate}`;
-        const overviewUrl = `${rohlikUrl}/overview/?key=${rohlikToken}&carrier=BEVY&month=${getDataDate}`;
-        const bonusesPenaltiesUrl =
-          `${rohlikUrl}/bonuses-penalties/?key=${rohlikToken}&carrier=BEVY&month=${getDataDate}`;
-
-        const [
-          invoicingResult,
-          overviewResult,
-          bonusesPenaltiesResult,
-        ] = await Promise.all([
-          axios.get(invoicingUrl),
-          axios.get(overviewUrl),
-          axios.get(bonusesPenaltiesUrl),
-        ]);
-
-        const previousDayDateIso = subDays(dateNow, 1).toISOString();
-
-        const invoicing = csvToJsonStyle(invoicingResult.data);
-        const overview = csvToJsonStyle(overviewResult.data);
-        const bonusesPenalties = csvToJsonStyle(bonusesPenaltiesResult.data);
-
-        const data = {
-          _id: previousDayDateIso,
-          overview,
-          invoicing,
-          bonusesPenalties,
-        } as unknown as RohlikData;
-
-        await insertData(data);
+        const data = await getReportsData(BevyRohlikCarriers.BEVY_PRAGUE, new Date().getTime());
+        await pragReportsInsertData(data);
       } catch (e) {
-        functions.logger.log("getCouriersDataAndSaveToDb:Error:  ", e);
+        functions.logger.log("getCourierReportsAndSavePrague:error:  ", e);
+      }
+    });
+
+export const getCourierReportsAndSavePlzen = functions
+    .region("europe-west3")
+    .pubsub
+    .schedule("00 03 * * *")
+    .timeZone("Europe/Prague")
+    .onRun(async (context) => {
+      functions.logger.log("getCourierReportsAndSavePlzen:context ", context.timestamp);
+
+      try {
+        const data = await getReportsData(BevyRohlikCarriers.BEVY_PLZEN, new Date().getTime());
+        await plzenReportsInsertData(data);
+      } catch (e) {
+        functions.logger.log("getCourierReportsAndSavePlzen:error:  ", e);
       }
     });
