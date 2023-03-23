f744791e9f6b125be2557b142cfbd8c73c044902
Jan Hujsa <jan.hujsa@gmail.com>
2023-03-23 12:21:08 +0100

    Add logs

--- functions/src/functions/utils/generateTaskOrder.ts
+++ functions/src/functions/utils/generateTaskOrder.ts
@@ -1,4 +1,5 @@
 import { groupBy } from "lodash";
+import { logger } from "firebase-functions";
 
 import { OriginOnFleetTask, TaskMetadata } from "../../types/tasks";
 import { HelperTask } from "../types";
@@ -27,15 +28,20 @@ export const generateOrderForTasks = (onFleetTasks: OriginOnFleetTask[]) => {
       .filter((t): t is HelperTask =>
         t.userId && t.worker && t.estimatedArrivalTime
       );
+  logger.log("generateOrderForTasks:helperTasks ", helperTasks);
 
   // TODO: is it possible to improve typing to remove type assertions?
   const sortedHelperTasks = sortByWorkerAndEat(helperTasks) as HelperTask[];
+  logger.log("generateOrderForTasks:sortedHelperTasks ", helperTasks);
 
   const groupedByUser = groupBy(sortedHelperTasks, "userId");
+  logger.log("generateOrderForTasks:groupedByUser ", helperTasks);
+
   const groupedByUserAndWorker = Object.keys(groupedByUser).reduce<GroupedByUserAndWorker>((total, key) => ({
     ...total,
     [key]: groupBy(groupedByUser[key], "worker"),
   }), {});
+  logger.log("generateOrderForTasks:groupedByUserAndWorker ", helperTasks);
 
   return onFleetTasks.map((onFleetTask) => {
     const userId = getMetadataValueByName(onFleetTask.metadata, TaskMetadata.UserId);
--- functions/src/functions/utils/getMetadataValueByName.ts
+++ functions/src/functions/utils/getMetadataValueByName.ts
@@ -4,6 +4,6 @@ import { OnfleetMetadata } from "@onfleet/node-onfleet/metadata";
 import { TaskMetadata } from "../../types/tasks";
 
 export const getMetadataValueByName = (metadata: OnfleetMetadata[], name: TaskMetadata) => {
-  logger.log("getMetadataValueByName:metadata ", metadata, " metadata:name ", name);
+  logger.log("getMetadataValueByName:metadata ", metadata, " | name ", name);
   return metadata.find((m) => m.name === name)?.value;
 };
